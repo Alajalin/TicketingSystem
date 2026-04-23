c3bf5ae16a128d2d4da2e6084227f1c5e2150481
Faisal <Alajalin.fs@gmail.com>
2026-04-23 09:44:46 +0300

    Fix Swagger JWT configuration - downgrade Swashbuckle to v9

--- TicketingSystem.API/Program.cs
+++ TicketingSystem.API/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using System.Text;
+using Microsoft.OpenApi.Models;
 using TicketingSystem.Core.Interfaces;
 using TicketingSystem.DataAccess.Context;
 using TicketingSystem.DataAccess.Repositories;
@@ -21,7 +22,32 @@ builder.Host.UseSerilog();
 // Controllers
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+	{
+		Name = "Authorization",
+		Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+		Scheme = "Bearer",
+		BearerFormat = "JWT",
+		In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+		Description = "Enter: Bearer {your token}"
+	});
+	c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+	{
+		{
+			new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+			{
+				Reference = new Microsoft.OpenApi.Models.OpenApiReference
+				{
+					Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			new string[] {}
+		}
+	});
+});
 
 // Database
 builder.Services.AddDbContext<AppDbContext>(options =>
